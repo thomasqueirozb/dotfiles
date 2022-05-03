8c67957bbccc26bd160992fca41a11f3396feb9f
Thomas Queiroz <thomasqueirozb@gmail.com>
2022-05-03 10:18:21 -0300

    polybar: improve install.sh + fix font awesome

--- .config/polybar/install.sh
+++ .config/polybar/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # fonts
+if test paru; then
+    aur_helper=paru
+elif test yay; then
+    aur_helper=yay
+fi
 
-yay -S --needed --noconfirm ttf-material-design-icons
+$aur_helper -S --needed --noconfirm ttf-material-design-icons
 sudo pacman -S --needed --noconfirm ttf-font-awesome pacman-contrib
 
